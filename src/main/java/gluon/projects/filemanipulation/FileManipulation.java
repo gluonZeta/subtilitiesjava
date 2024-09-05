8469b6e9c6b901911592e67d5aee56038e201531
Gluon <andrainat@gmail.com>
2024-09-05 08:01:41 +0200

    Add element to read me file about reading big file, stackoverflow, heap space

--- src/main/java/gluon/projects/filemanipulation/FileManipulation.java
+++ src/main/java/gluon/projects/filemanipulation/FileManipulation.java
@@ -4,6 +4,12 @@ import gluon.projects.utilities.PropertiesGetter;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 @Getter
@@ -15,6 +21,42 @@ public class FileManipulation {
     public FileManipulation() {
         Properties properties = PropertiesGetter.getProperties("application.properties");
         this.filePath = properties.getProperty("tinystorypath");
+    }
+
+    public List<String> getFileLines() {
+        List<String> lines = new ArrayList<>();
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(this.filePath))) {
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+
+            Runtime runtime = Runtime.getRuntime();
+            long totalMemory = runtime.totalMemory();
+            long freeMemory = runtime.freeMemory();
+            long usedMemory = totalMemory - freeMemory;
+
+            double totalMems = totalMemory/Math.pow(1024, 3);
+            double freeMems = freeMemory/Math.pow(1024, 3);
+            double usedMems = usedMemory/Math.pow(1024, 3);
+
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println(totalMems);
+            System.out.println(freeMems);
+            System.out.println(usedMems);
+            System.out.println("-------------------------------------------------------------------------");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
+    }
+
+    public int stackOverflowMethod(int x) {
+        x = x+1;
+        return stackOverflowMethod(x);
     }
 
 }
--- src/test/java/gluon/projects/filemanipulation/FileManipulationTest.java
+++ src/test/java/gluon/projects/filemanipulation/FileManipulationTest.java
@@ -1,16 +1,35 @@
 package gluon.projects.filemanipulation;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class FileManipulationTest {
 
+    FileManipulation fileManipulation;
+
+    @BeforeEach
+    void setUp() {
+        this.fileManipulation = new FileManipulation();
+    }
+
     @Test
     public void testInstanciation() {
-        FileManipulation fileManipulation = new FileManipulation();
-        Assertions.assertNotNull(fileManipulation);
+        Assertions.assertNotNull(this.fileManipulation);
         Assertions.assertEquals("C:\\Users\\andra\\Documents\\my_project\\data\\train.csv",
-                fileManipulation.getFilePath());
+                this.fileManipulation.getFilePath());
+    }
+
+    public void testReadFile() {
+        List<String> lines = this.fileManipulation.getFileLines();
+        Assertions.assertNotNull(lines);
+    }
+
+    @Test
+    public void testStackOverflow() {
+        this.fileManipulation.stackOverflowMethod(23);
     }
 
 }
